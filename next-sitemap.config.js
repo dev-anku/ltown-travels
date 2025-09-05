7039c64abb6d963c9299130ab676c52979671c36
Anku <ankitkumarmuhal3@gmail.com>
2025-09-05 19:17:47 +0530

    Added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://ltown-travels.netlify.app", // replace with your domain
+  generateRobotsTxt: true, // also generate robots.txt
+};
