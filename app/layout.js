05869fca7e1093171a5b773f5e6a2587d332bd64
Anku <ankitkumarmuhal3@gmail.com>
2026-03-01 21:39:57 +0530

    fix: new tag manager account

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P6MBZ9PF');`}
+})(window,document,'script','dataLayer','GTM-5HDHZT2L');`}
         </Script>
         {/* <!-- End Google Tag Manager --> */}
       </head>
@@ -43,7 +43,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* <!-- Google Tag Manager (noscript) --> */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P6MBZ9PF"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5HDHZT2L"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
